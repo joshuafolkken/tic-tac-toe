5d9c8835d2218bc7f71e2306689a8048b85c4e5e
Joshua Folkken <joshuafolkken@gmail.com>
2024-05-19 14:29:44 +0900

    Circle #3

---
+++ circle.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var radius := 65.0
+@export var line_width := 15.0
+@export var color := Color8(148, 188, 240)
+
+
+func _draw() -> void:
+	draw_arc(Vector2.ZERO, radius, 0, TAU, 360, color, line_width, true)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
