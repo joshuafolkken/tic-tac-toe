0efec8935dcbee2b7dcb8c6cad1e0b2cfc0aaa10
Joshua Folkken <joshuafolkken@gmail.com>
2024-05-21 17:27:20 +0900

    Button Index #39

---
+++ cell_button_2d.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var button_index := 0
+
+@onready var cell_button: Button = $CellButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cell_button.pressed.connect(_on_button_pressed)
+
+
+func _on_button_pressed() -> void:
+	var cell_line := get_parent() as CellLine
+	var line_index = cell_line.line_index
+
+	print("cell_line_index: %d, button_index: %d" % [line_index, button_index])
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
