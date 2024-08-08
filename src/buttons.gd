ed719753a837d91c6e66f294da53dba8e5ebd524
Joshua Folkken <joshuafolkken@gmail.com>
2024-08-08 17:50:27 +0900

    Refactoring #68 ensure not null

--- src/buttons.gd
+++ src/buttons.gd
@@ -4,6 +4,9 @@ var _data: Array[CellButton2D] = []
 
 
 func append(button: CellButton2D) -> void:
+	if not Global.ensure_not_null(button, "Button"):
+		return
+
 	_data.append(button)
 
 
@@ -17,7 +20,18 @@ func reset_all() -> void:
 		button.reset()
 
 
+func validate_position(position: Vector2i) -> void:
+	if not Global.ensure_not_null(position, "Position"):
+		return
+
+	if position.x < 0 or position.x > 2 or position.y < 0 or position.y > 2:
+		Global.throw_exception("Position is out of bounds. x: %s, y: %s" % [position.x, position.y])
+		return
+
+
 func from_vector2i(position: Vector2i) -> CellButton2D:
+	validate_position(position)
+
 	var index = position.x * 3 + position.y
 	return _data[index]
 
