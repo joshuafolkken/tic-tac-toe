3d2397a67b0e6d8e72f9ad5aeb99e1033f838850
joshuafolkken <joshuafolkken@users.noreply.github.com>
2024-09-17 04:49:30 +0000

    deploy: 2f1784fc275e91929afa35109a6c80940eb865a6

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1726546508|6469444';
+const CACHE_VERSION = '1726548568|10191770';
 /** @type {string} */
 const CACHE_PREFIX = 'Tic Tac Toe-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
